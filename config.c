764c13a89dfabf8fc2869a25c341f986c41d5118
Aparna Rajpurkar <aparna.arr@gmail.com>
2023-04-13 01:16:25 -0400

    edits to bar for testing

--- config.c
+++ config.c
@@ -4,16 +4,21 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-mail",    600,  1 },
-    {"sb-music",   0,    2 },
+/*
+    {"sb-internet",    5,    2},
+    {"sb-nettraf",    1,    6},
+//    
     {"sb-disk",    1800, 3 },
     {"sb-memory",  10,   4 },
-    {"sb-loadavg", 5,    5 },
-    {"sb-mic",     0,    6 },
-    {"sb-record",  0,    7 },
+    {"sb-cpu",    10,    5},
+//
+//
     {"sb-volume",  0,    8 },
+*/
     {"sb-battery", 5,    9 },
-    {"sb-date",    1,    10},
+    {"sb-clock",    60,    1},
+//
+
 };
 
 const unsigned short blockCount = LEN(blocks);
