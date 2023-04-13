d766097b0897d51cea9ecf07e983127e1d4ce13d
Aparna Rajpurkar <aparna.arr@gmail.com>
2023-04-13 11:15:22 -0400

    remove some status blocks

--- config.c
+++ config.c
@@ -6,11 +6,11 @@
 Block blocks[] = {
 
     {"sb-internet",    5,    2},
-    {"sb-nettraf",    1,    6},
+//    {"sb-nettraf",    1,    6},
 //    
     {"sb-disk",    1800, 3 },
-    {"sb-memory",  10,   4 },
-    {"sb-cpu",    10,    5},
+//    {"sb-memory",  10,   4 },
+//    {"sb-cpu",    10,    5},
 //
 //
     {"sb-volume",  0,    8 },
